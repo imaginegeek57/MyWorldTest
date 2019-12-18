0c3ec19eecd17f9a91912ef05d69df1ab3fdf0c3
imaginegeek57 <imaginegeek57@gmail.com>
2019-12-18 23:29:18 +0300

    update5

--- src/main/java/ru/pflb/at/page/MusicPage.java
+++ src/main/java/ru/pflb/at/page/MusicPage.java
@@ -2,6 +2,7 @@ package ru.pflb.at.page;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.pflb.at.techno.SWDriver;
 
 public class MusicPage extends BasePage {
@@ -15,22 +16,116 @@ public class MusicPage extends BasePage {
     }
 
     /**
+     *
+     */
+    @FindBy(xpath = "//textarea[@placeholder='Расскажите о новом событии...']")
+    private WebElement buttonTell;
+
+    /**
+     *
+     */
+    @FindBy(xpath = "//div/div[text()='Музыка']")
+    private WebElement buttonMus;
+
+    /**
+     * Кнопка "Ещё"
+     */
+    @FindBy(xpath = "//i[@class='b-history_filter-more__icon ui-icon']")
+    private WebElement buttonElse;
+
+    /**
      * Кнопка "Музыка"
      */
-    @FindBy(xpath = "//li[@data-id='music']/a")
+    @FindBy(xpath = "//li/div[text()='Музыка']")
     private WebElement buttonMusic;
 
     /**
-     * Текстовое поле "search music"
+     * Кнопка добавить музыку из поиска
      */
-    @FindBy(css = "//input[@name='query']")
-    private WebElement searchMusic;
+    @FindBy(css = "//button[@class='b-audio-upload__link__button b-audio-upload__link__button-search ui-button-gray']")
+    private WebElement addMusic;
 
     /**
-     * Кнопка "Enter"
+     * Текстовое поле "Поиск"
      */
-    @FindBy(xpath = "//button")
-    private WebElement buttonEnterSearch;
+    @FindBy(xpath = "//input[@placeholder='Поиск']")
+    private WebElement searchMus;
 
+    /**
+     * Веб элемент "tracks list"
+     */
+    @FindBy(xpath = "//div[@class='b-audio-upload__search-form__content__list']")
+    private WebElement tracksList;
 
+    /**
+     * Бокс отметки выбранных треков
+     */
+    @FindBy(xpath = "//div[@class='b-audio-upload__search-form__content__list']/div/div/i")
+    private WebElement checkBox;
+
+    /**
+     * Кнопка добавить музыку из поиска
+     */
+    @FindBy(xpath = "//button[text()='Добавить']")
+    private WebElement addTrack;
+
+    /**
+     * Жмем кнопку 'Музыка'
+     */
+    public MusicPage enterMusic() {
+        buttonMusic.click();
+        LOG.info("Жмем кнопку 'Музыка'");
+        screenshot();
+        return this;
+    }
+
+    /**
+     * Жмем кнопку 'Ещё'
+     */
+    public MusicPage pressElse() {
+        buttonElse.click();
+        LOG.info("Жмем кнопку 'Ещё'");
+        screenshot();
+        return this;
+    }
+
+    /**
+     * Жмем кнопку 'Добавить из поиска'
+     */
+    public MusicPage addMusicFrom() {
+        new WebDriverWait(getWebDriver(), 10);
+        addMusic.click();
+        LOG.info("Жмем кнопку 'Добавить из поиска'");
+        screenshot();
+        return this;
+    }
+
+    /**
+     * Вводим название музыки для поиска
+     */
+    public MusicPage searchMusic(String search) {
+        searchMus.sendKeys(search);
+        LOG.info("В поле 'Поиск' записано: {}", search);
+        return this;
+    }
+
+    /**
+     * Отмечаем выбранные треки
+     */
+    public MusicPage enterSearch() {
+        checkBox.click();
+        LOG.info("Треки выбранны");
+        screenshot();
+        return this;
+    }
+
+    /**
+     * Жмем кнопку 'Добавить выбранные треки'
+     */
+    public MusicPage addTracks() {
+        addTrack.click();
+        LOG.info("Жмем кнопку 'Добавить выбранные треки'");
+        screenshot();
+        return this;
+    }
 }
--- src/main/java/ru/pflb/at/test/AddMusicTest.java
+++ src/main/java/ru/pflb/at/test/AddMusicTest.java
@@ -9,7 +9,7 @@ public class AddMusicTest extends BaseTest {
     /**
      * URL картинки
      */
-    public static final String MUSIC_URL = "50 cent";
+    public static final String IMAGE_URL = "https://avatars.mds.yandex.net/get-pdb/1976538/7ea9875b-c3cb-49ff-9ab3-9dc436be6ab4/s1200";
 
     @BeforeEach
     public void setUp() {
@@ -18,8 +18,14 @@ public class AddMusicTest extends BaseTest {
     }
 
     @Test
-    public void upload_photo_by_link() {
-        musicPage.screenshot();
-
+    public void add_music_track_test() {
+        musicPage
+                .pressElse()
+                .enterMusic()
+                .addMusicFrom()
+                .searchMusic("50 cent")
+                .enterSearch()
+                .addTracks();
     }
+
 }
